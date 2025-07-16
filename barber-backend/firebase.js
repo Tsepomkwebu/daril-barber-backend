2371bbbcb80f6459438cc57efa5b2e4220f5cab9
Tsepo Mkwebu <tsepomkwebu@gmail.com>
2025-07-16 16:26:22 +0200

    Add firebase integration and webhook fix

--- barber-backend/firebase.js
+++ barber-backend/firebase.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 
 // Load service account from local JSON file (make sure it's in .gitignore!)
-const serviceAccount = require('./firebase-service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
